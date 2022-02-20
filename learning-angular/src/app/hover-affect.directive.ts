6c9cbd766d10668cc1d45c1f30750c814ec96448
sp0414 <90412281+sp414@users.noreply.github.com>
2022-02-20 18:59:02 -0500

    chnge

--- learning-angular/src/app/hover-affect.directive.ts
+++ learning-angular/src/app/hover-affect.directive.ts
@@ -24,7 +24,7 @@ export class HoverAffectDirective {
  */
 
 
-/* 
+
 import { Directive, HostListener, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
@@ -33,9 +33,10 @@ import { Directive, HostListener, ElementRef, Input, OnInit } from '@angular/cor
 export class HoverAffectDirective {
   constructor(public elementRef:ElementRef) { }
   @Input('hover-affect') hoverAffect:any;  
-//   ngOnInit(){
-//     console.log(this.hoverAffect)
-//  }
+
+  ngOnInit(){
+  console.log(this.hoverAffect)
+ }
   
   @HostListener('mouseenter') onMouseEnter() {
 
@@ -48,70 +49,7 @@ export class HoverAffectDirective {
     this.elementRef.nativeElement.classList.remove(this.hoverAffect);
 }
 } 
- */
-
-/* import { Directive, HostListener, ElementRef, Input, OnInit } from '@angular/core';
-
-@Directive({
-  selector: '[hover-affect]'
-})
-
-  export class HoverAffectDirective {
-
-    constructor(private el: ElementRef) { }
-  
-    @HostListener('mouseenter') onMouseEnter() {
-      this.highlight('yellow');
-    }
-  
-    @HostListener('mouseleave') onMouseLeave() {
-      this.highlight('');
-    }
-  
-    private highlight(color: string) {
-      this.el.nativeElement.style.backgroundColor = color;
-    }
-  
-  }
-     */ 
-
-/* import { Directive, HostListener, ElementRef, Input, OnInit } from '@angular/core';
-
-@Directive({
-  selector: '[hover-affect]'
-})
-
-  export class HoverAffectDirective {
-
-    constructor(private el: ElementRef) { }
+ 
 
 
-  @HostListener('mouseenter') onMouseEnter() {
-    
-    this.el.nativeElement.addClass = 'hve';
-  }
 
-  @HostListener('mouseleave') onMouseLeave() {
-    
-    this.el.nativeElement.addClass = 'hve';
-  } 
-
-} */ 
-
-
-import { Directive, HostListener, ElementRef, Input, OnInit } from '@angular/core';
-
-@Directive({
-  selector: '[hover-affect]',
-  templateUrl: 'content-list.component.html',
-
-  export class HoverAffectDirective {
-
-   mouseEnter(div : string){
-      console.log("mouse enter : " + div);
-   }
-
-   mouseLeave(div : string){
-     console.log('mouse leave :' + div);
-   }
-}
