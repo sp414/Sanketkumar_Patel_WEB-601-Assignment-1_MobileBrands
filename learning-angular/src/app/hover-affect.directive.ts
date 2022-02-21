d142470e5a322daff035b9b58ccc850823522a08
sp0414 <90412281+sp414@users.noreply.github.com>
2022-02-20 19:06:10 -0500

    remove comments

--- learning-angular/src/app/hover-affect.directive.ts
+++ learning-angular/src/app/hover-affect.directive.ts
@@ -1,28 +1,3 @@
-/* import { Directive, HostListener, ElementRef, Input, OnInit } from '@angular/core';
-
-@Directive({
-  selector: '[hover-affect]'
-})
-export class HoverAffectDirective {
-  constructor(public elementRef:ElementRef) { }
-  @Input('hover-affect') hoverAffect:any;  
-//   ngOnInit(){
-//     console.log(this.hoverAffect)
-//  }
-  
-  @HostListener('mouseenter') onMouseEnter() {
-
-    this.elementRef.nativeElement.classList.add(this.hoverAffect);
-    
-  
- }
-
-  @HostListener('mouseleave') onMouseLeave() {
-    this.elementRef.nativeElement.classList.remove(this.hoverAffect);
-}
-} 
- */
-
 
 
 import { Directive, HostListener, ElementRef, Input, OnInit } from '@angular/core';
